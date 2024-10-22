ff64a59dfc6a57782ea2cf67fc11866880819581
nmkzzztos <nmkzzztos@users.noreply.github.com>
2024-10-22 09:44:53 +0000

    deploy: 06dda72395ea49abe5c3db2a3a1d097f76ec8498

---
+++ assets/js/c15d9823.36eef2c2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrag_chat_bot_docs=self.webpackChunkrag_chat_bot_docs||[]).push([[146],{9328:t=>{t.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ en/assets/js/22f9ffc3.2ee48bcb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrag_chat_bot_docs=self.webpackChunkrag_chat_bot_docs||[]).push([[782],{6004:t=>{t.exports=JSON.parse('{"metadata":{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
